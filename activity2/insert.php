470ceeec5e8c80ef12e545a5ae6257c80990fcb4
Leokanette Cailing <79577262+Leokanette@users.noreply.github.com>
2024-02-22 16:27:18 +0800

    last act

---
+++ activity2/insert.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Firstname</label>
+    <input type="fname" class="form-control" name='fname' id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter firstname">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Middlename</label>
+    <input type="mname" class="form-control" name='mname' id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter middlename">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Lastname</label>
+    <input type="lname" class="form-control" name='lname' id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter lastname">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Year</label>
+    <input type="year" class="form-control" name='year' id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter year">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Section</label>
+    <input type="section" class="form-control" name='section' id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter section">
+  </div>
+  <button type="submit" class="btn btn-primary">INSERT</button>
+</form>
+
+
+
+
+
+
+</body>
+</html>
+
